aeaff38dcf994f6ac742b436e5586ebb45acbb40
linkbroken <damouramory@gmail.com>
2024-07-18 15:44:07 +0300

    finished navbar setting, added react router, added font awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 import { Footer, Header } from './containers'
 import { Navbar, Product } from './componentes'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route element={<Header/>} path='/'/>
+         
+      </Routes>
+    </Router>
     </>
   )
 }
