c92de13dd106282c7a640ba04d4c703243a26933
MHD-Ammar <62371028+MHD-Ammar@users.noreply.github.com>
2024-07-18 14:24:53 +0300

    create the porject

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Footer, Header } from './containers'
+import { Navbar, Product } from './componentes'
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
